b0546d46da076914ff0e9457fac2c065ba3f1195
thunderg5 <salmanit2012@gmail.com>
2015-03-06 16:58:06 +0000

    Adding Account Activation and User Mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
     before_save { self.email = email.downcase }
+    before_create :downcase_email
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
@@ -36,5 +37,18 @@ class User < ActiveRecord::Base
     def authenticated?(remember_token)
         return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+    
+    private
+    
+    # Converts email to all lower-case.
+    def downcase_email
+        self.email = email.downcase
+    end
+    
+    # Create and assigns the activation token and digest.
+    def create_activation_digest
+        self.activation_token  = User.new_token
+        self.activation_digest = User.digest(activation_token)
     end
 end
